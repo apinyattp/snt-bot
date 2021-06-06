aab844adbafb9fbc88fe2600d9ebf0e73b0877a2
mheawmeaou <mheawmeaou@mheawmeaous-MacBook-Air.local>
2021-06-06 14:50:37 +0700

    ff

--- facebook-group-posts-scraper-public/src/index.js
+++ facebook-group-posts-scraper-public/src/index.js
@@ -250,7 +250,6 @@ function generateFacebookGroupUrlFromId(groupId) {
 async function createBrowser(arguments) {
   const browserOptions = {
     headless: arguments['headful'] === false,
-    userDataDir: './pupeeteer_data',
     args: [
       '--no-sandbox',
       '--disable-setuid-sendbox',
@@ -667,9 +666,8 @@ async function main(
     let page = await browser.newPage();
     // let page = await incognitoMode(browser);
     await page.setUserAgent("User agent Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.0 Safari/537.36");
-    // page = await facebookLogIn(arguments, page, setPageListeners);
+    page = await facebookLogIn(arguments, page, setPageListeners);
     // for (var i = 0; i < facebookGroupIdList.length; i++) {
-     await setPageListeners(page);
     for (let i = 0; i < facebookGroupIdList.length; i++) {
       const id = facebookGroupIdList[i];
       const groupUrl = generateFacebookGroupUrlFromId(id);
